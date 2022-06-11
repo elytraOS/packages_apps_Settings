Project-Awaken/android_packages_apps_Settings

Settings: Add pref for qs brightness slider position [2/2]
Change-Id: I583e652fad3e4f63182bab13a73188f35b8c4410
Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

src/com/android/settings/awaken/fragments/BrightnessSliderSettings.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2020 YAAP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.awaken.fragments;
+
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+public class BrightnessSliderSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.statusbar_settings_brightness_slider);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.AWAKEN;
+    }
+}

src/com/android/settings/awaken/fragments/DisplayCustomizations.java
@@ -16,6 +16,7 @@
 
 package com.android.settings.awaken.fragments;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.ContentResolver;
 import android.content.res.Resources;
@@ -35,6 +36,7 @@ import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import com.awaken.support.preferences.SecureSettingMasterSwitchPreference;
 import com.awaken.support.preferences.SecureSettingSwitchPreference;
 
 public class DisplayCustomizations extends SettingsPreferenceFragment
@@ -45,6 +47,7 @@ public class DisplayCustomizations extends SettingsPreferenceFragment
     private static final String CONFIG_RESOURCE_NAME = "flag_combined_status_bar_signal_icons";
     private static final String PREF_STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
     private static final String PREF_STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+    private static final String BRIGHTNESS_SLIDER = "qs_show_brightness";
 
     private static final int BATTERY_STYLE_PORTRAIT = 0;
     private static final int BATTERY_STYLE_TEXT = 4;
@@ -54,6 +57,8 @@ public class DisplayCustomizations extends SettingsPreferenceFragment
     //private static final int BATTERY_PERCENT_SHOW_OUTSIDE = 2;
 
     private static final String COBINED_STATUSBAR_ICONS = "show_combined_status_bar_signal_icons";
+
+    private SecureSettingMasterSwitchPreference mBrightnessSlider;
 
     private ListPreference mBatteryPercent;
     private ListPreference mBatteryStyle;
@@ -105,6 +110,13 @@ public class DisplayCustomizations extends SettingsPreferenceFragment
                 COBINED_STATUSBAR_ICONS, def ? 1 : 0) == 1;
         mCombinedIcons.setChecked(enabled);
         mCombinedIcons.setOnPreferenceChangeListener(this);
+
+        mBrightnessSlider = (SecureSettingMasterSwitchPreference)
+                findPreference(BRIGHTNESS_SLIDER);
+        mBrightnessSlider.setOnPreferenceChangeListener(this);
+        enabled = Settings.Secure.getInt(resolver,
+                BRIGHTNESS_SLIDER, 1) == 1;
+        mBrightnessSlider.setChecked(enabled);
     }
 
     @Override
@@ -132,6 +144,11 @@ public class DisplayCustomizations extends SettingsPreferenceFragment
             boolean enabled = (boolean) newValue;
             Settings.Secure.putInt(resolver,
                     COBINED_STATUSBAR_ICONS, enabled ? 1 : 0);
+            return true;
+        } else if (preference == mBrightnessSlider) {
+            Boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(resolver,
+                    BRIGHTNESS_SLIDER, value ? 1 : 0);
             return true;
         }
         return false;

elytraOS/packages_apps_Settings

src/com/android/settings/elytra/fragments/BrightnessSliderSettings.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2020 YAAP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.elytra.fragments;
+
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+public class BrightnessSliderSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.statusbar_settings_brightness_slider);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.ELYTRA;
+    }
+}

src/com/android/settings/elytra/fragments/DisplayCustomizations.java
@@ -16,6 +16,7 @@
 
 package com.android.settings.elytra.fragments;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.ContentResolver;
 import android.content.res.Resources;
@@ -35,6 +36,7 @@ import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import com.elytra.support.preferences.SecureSettingMasterSwitchPreference;
 import com.elytra.support.preferences.SecureSettingSwitchPreference;
 
 public class DisplayCustomizations extends SettingsPreferenceFragment
@@ -45,6 +47,7 @@ public class DisplayCustomizations extends SettingsPreferenceFragment
     private static final String CONFIG_RESOURCE_NAME = "flag_combined_status_bar_signal_icons";
     private static final String PREF_STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
     private static final String PREF_STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+    private static final String BRIGHTNESS_SLIDER = "qs_show_brightness";
 
     private static final int BATTERY_STYLE_PORTRAIT = 0;
     private static final int BATTERY_STYLE_TEXT = 4;
@@ -54,6 +57,8 @@ public class DisplayCustomizations extends SettingsPreferenceFragment
     //private static final int BATTERY_PERCENT_SHOW_OUTSIDE = 2;
 
     private static final String COBINED_STATUSBAR_ICONS = "show_combined_status_bar_signal_icons";
+
+    private SecureSettingMasterSwitchPreference mBrightnessSlider;
 
     private ListPreference mBatteryPercent;
     private ListPreference mBatteryStyle;
@@ -105,6 +110,13 @@ public class DisplayCustomizations extends SettingsPreferenceFragment
                 COBINED_STATUSBAR_ICONS, def ? 1 : 0) == 1;
         mCombinedIcons.setChecked(enabled);
         mCombinedIcons.setOnPreferenceChangeListener(this);
+
+        mBrightnessSlider = (SecureSettingMasterSwitchPreference)
+                findPreference(BRIGHTNESS_SLIDER);
+        mBrightnessSlider.setOnPreferenceChangeListener(this);
+        enabled = Settings.Secure.getInt(resolver,
+                BRIGHTNESS_SLIDER, 1) == 1;
+        mBrightnessSlider.setChecked(enabled);
     }
 
     @Override
@@ -132,6 +144,11 @@ public class DisplayCustomizations extends SettingsPreferenceFragment
             boolean enabled = (boolean) newValue;
             Settings.Secure.putInt(resolver,
                     COBINED_STATUSBAR_ICONS, enabled ? 1 : 0);
+            return true;
+        } else if (preference == mBrightnessSlider) {
+            Boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(resolver,
+                    BRIGHTNESS_SLIDER, value ? 1 : 0);
             return true;
         }
         return false;